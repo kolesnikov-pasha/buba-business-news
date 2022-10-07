d7c54c983310974f50a1fd13fb302f4fef94e098
Kolesnikov Pavel <olpyhn@gmail.com>
2022-10-07 23:46:13 +0200

    Added klerk parsing

--- news_parsing/url_parser.py
+++ news_parsing/url_parser.py
@@ -1,11 +1,13 @@
 import json
 from news_parser import parse_news
 from consultant_plus.url_parser import parse_consultant_plus_all
+from klerk.url_parser import parse_klerk_all
 from tqdm import tqdm
 
 
 __PARSERS__ = {
-    "consultant_plus": parse_consultant_plus_all
+    "consultant_plus": parse_consultant_plus_all,
+    "klerk": parse_klerk_all
 }
 
 
@@ -17,9 +19,9 @@ def parse_all_sources():
             print(f"Start parsing {source['name'].upper()}")
             urls = __PARSERS__[source["name"]](source["base_url"])
             for url in tqdm(urls):
-                parse_news(source["name"], url)
+                result.append(parse_news(source["name"], url))
     return result
 
 
 
-parse_all_sources()
+print(parse_all_sources())
