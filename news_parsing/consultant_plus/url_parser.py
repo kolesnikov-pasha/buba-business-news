d54c8d72a6e9b68f3e829b0447a291edb24476cf
Kolesnikov Pavel <olpyhn@gmail.com>
2022-10-08 04:03:34 +0200

    Refactored news parsing service to make it more reusable, added auto-upload on Google Cloud Storage

--- news_parsing/consultant_plus/url_parser.py
+++ news_parsing/consultant_plus/url_parser.py
@@ -1,30 +1,40 @@
 import bs4 as bs
 import requests
+from news_parser import NewsParser
 
 
-def load_url(url):
-    return requests.get(url).content
+class ConsultantParser(NewsParser):
+    def __init__(self):
+        super().__init__()
+        self.parser_version = 1
 
 
-def parse_consultant_plus_all(base_url):
-    years = [2022]
-    months = {
-        2020: list(range(1, 13)),
-        2021: list(range(1, 13)),
-        2022: list(range(6, 11))
-    }
-    urls = []
-    for year in years:
-        for month in months[year]:
-            urls += parse_consultant_plus(base_url, f"legalnews/chronomap/{year}/{month}")
-    return urls
+    @staticmethod
+    def parse_all_pages_urls(base_url):
+        years = [2022]
+        months = {
+            2020: list(range(1, 13)),
+            2021: list(range(1, 13)),
+            2022: list(range(6, 11))
+        }
+        urls = []
+        for year in years:
+            for month in months[year]:
+                urls += ConsultantParser.parse_page_urls(base_url, f"legalnews/chronomap/{year}/{month}")
+        return urls
 
 
-def parse_consultant_plus(base_url, news_page_url):
-    page = bs.BeautifulSoup(load_url(base_url + news_page_url), "html.parser")
-    listing = page.find("div", attrs={"class": "archive-month__listing"})
-    urls = []
-    for item in listing.find_all("div", attrs={"class": "archive-month__item"}):
-        href = item.find("a", attrs={"class": "archive-month__item-title"})["href"]
-        urls.append(base_url + href[1:])
-    return urls
+    @staticmethod
+    def parse_page_urls(base_url, news_page_url):
+        page = bs.BeautifulSoup(ConsultantParser.load_url(base_url + news_page_url), "html.parser")
+        listing = page.find("div", attrs={"class": "archive-month__listing"})
+        urls = []
+        for item in listing.find_all("div", attrs={"class": "archive-month__item"}):
+            href = item.find("a", attrs={"class": "archive-month__item-title"})["href"]
+            urls.append(base_url + href[1:])
+        return urls
+
+    
+    @staticmethod
+    def url_to_id(url):
+        return url[len("http://www.consultant.ru/legalnews/"):-1]
--- news_parsing/tinkoff/url_parser.py
+++ news_parsing/tinkoff/url_parser.py
@@ -1,23 +1,33 @@
 import bs4 as bs
 import requests
 
-
-def load_url(url):
-    return requests.get(url).content
+from news_parser import NewsParser
 
 
-def parse_tinkoff_all(base_url):
-    pages = list(range(1, 2))
-    urls = []
-    for page in pages:
-        urls += parse_tinkoff(base_url, f"flows/goskontrol/page/{page}/")
-    return urls
+class TinkoffParser(NewsParser):
+    def __init__(self):
+        super().__init__()
+        self.parser_version = 1
+
+    @staticmethod
+    def url_to_id(url):
+        return url[len("https://journal.tinkoff.ru/news/"):-1]
 
 
-def parse_tinkoff(base_url, news_page_url):
-    page = bs.BeautifulSoup(load_url(base_url + news_page_url), "html.parser")
-    urls = []
-    for item in page.find_all("div", attrs={"class": "card--LxPHF"}):
-        href = item.find("a", attrs={"class": "link--xmoGM"})["href"]
-        urls.append(base_url + href[1:])
-    return urls
+    @staticmethod
+    def parse_all_pages_urls(base_url):
+        pages = list(range(1, 51))
+        urls = []
+        for page in pages:
+            urls += TinkoffParser.parse_page_urls(base_url, f"flows/goskontrol/page/{page}/")
+        return urls
+
+
+    @staticmethod
+    def parse_page_urls(base_url, news_page_url):
+        page = bs.BeautifulSoup(TinkoffParser.load_url(base_url + news_page_url), "html.parser")
+        urls = []
+        for item in page.find_all("div", attrs={"class": "card--LxPHF"}):
+            href = item.find("a", attrs={"class": "link--xmoGM"})["href"]
+            urls.append(base_url + href[1:])
+        return urls
