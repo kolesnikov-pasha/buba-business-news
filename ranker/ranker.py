9a3e245638e2fa5921d78f8b9c20847c7d110a75
Kolesnikov Pavel <olpyhn@gmail.com>
2022-10-08 23:46:09 +0200

    Added ranker boilerplate

---
+++ ranker/ranker.py
@@ -0,0 +1,20 @@
+USER_TYPES = [
+    "ceo",
+    "accountant"
+]
+
+
+def __get_cold_init_data__(user_type):
+    pass
+
+
+def create_user(user_type, user_id):
+    pass
+
+
+def get_rank(user_id, news_id):
+    pass
+
+
+def add_news_view(user_id, news_id):
+    pass
