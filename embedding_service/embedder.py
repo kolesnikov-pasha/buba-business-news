36d8a20e94f780f24e6b85c84456cee2bef13dd1
Ayvar Shakirov <snummus@gmail.com>
2022-10-08 03:15:44 +0300

    fix

--- embedding_service/embedder.py
+++ embedding_service/embedder.py
@@ -2,18 +2,6 @@ import torch
 from transformers import AutoTokenizer, AutoModel
 from models import NewsModel
 
-tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny")
-model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
-
-
-def embed_bert_cls(text, model, tokenizer):
-    t = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        model_output = model(**{k: v.to(model.device) for k, v in t.items()})
-    embeddings = model_output.last_hidden_state[:, 0, :]
-    embeddings = torch.nn.functional.normalize(embeddings)
-    return embeddings[0].cpu().numpy()
-
 
 class Embedder:
     tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny")
